44e861f3d0e63583b51ead6d667d69f72e89e6bc
Håkan Sundström <hakansundstrom@Hakans-MBP.lan>
2021-06-11 17:28:34 +0200

    default as modern

--- app/src/components/AppRoutes.tsx
+++ app/src/components/AppRoutes.tsx
@@ -39,12 +39,12 @@ const initRoutePush = (history: any, mode: Mode): void | null => {
   const historyPush = (path: string) => history.push(path + history.location.search);
 
   switch (mode) {
-    case Mode.MODERN:
-      historyPush(Paths.MODERN);
-      break;
     case Mode.CLASSIC:
-    default:
       historyPush(Paths.CLASSIC);
+      break;
+    case Mode.MODERN:
+    default:
+      historyPush(Paths.MODERN);
       break;
   }
 };
