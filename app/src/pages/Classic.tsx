6af3487c9c23a2b325795874bb9c36f10ebf6ccf
Håkan Sundström <hakansundstrom@Hakans-MBP.lan>
2021-06-17 12:32:59 +0200

    refactor css

--- app/src/pages/Classic.tsx
+++ app/src/pages/Classic.tsx
@@ -1,11 +1,33 @@
+/* eslint-disable max-len */
 import type { FC } from 'react';
 import WidgetContainer from 'components/WidgetContainer';
 import Content from 'components/Classic/Content';
 
-const Classic:FC = () => (
-  <WidgetContainer id="classic-container">
-    <Content />
-  </WidgetContainer>
-);
+const Classic:FC = () => {
+  createStyleTag();
+  return (
+    <WidgetContainer id="classic-container">
+      <Content />
+    </WidgetContainer>
+  );
+};
+
+const createStyleTag = () => {
+  const style = document.createElement('style');
+  style.innerText = `
+#classic-container > div > div.left.aligned.one.column.row > div > p {
+  display: flex;
+  align-items: center;
+}
+
+#classic-container .ui.grid > .row {
+  min-height: 25px;
+}
+#classic-container > div > div:nth-child(1) > div.eight.wide.column > div > div > div:nth-child(2) > div > i {
+  margin: 0 0 0 0.5em;
+}
+  `;
+  window.self.document.body.appendChild(style);
+};
 
 export default Classic;
--- app/src/pages/Modern.tsx
+++ app/src/pages/Modern.tsx
@@ -2,10 +2,45 @@ import type { FC } from 'react';
 import WidgetContainer from 'components/WidgetContainer';
 import Content from 'components/Modern/Content';
 
-const Modern:FC = () => (
-  <WidgetContainer id="modern-container">
-    <Content />
-  </WidgetContainer>
-);
+const Modern:FC = () => {
+  createStyleTag();
+  return (
+    <WidgetContainer id="modern-container">
+      <Content />
+    </WidgetContainer>
+  );
+};
+
+const createStyleTag = () => {
+  const style = document.createElement('style');
+  style.innerText = `
+#selections-modern-container .ui.disabled.input, .ui.input:not(.disabled) input[disabled] {
+  opacity: 1;
+}
+#selections-modern-container .ui.corner.label {
+  width: 2.8em;
+  padding-top: 2px;
+}
+#selections-modern-container > div:nth-child(2) > div > div.visible.menu.transition {
+  max-height: 150px!important;
+}
+#selections-modern-container > div:nth-child(2) > div > input[type=text] {
+  border-top-left-radius: 0px!important;
+  border-bottom-left-radius: 0px!important;
+  border-left-color: #e8e8e8;
+}
+#selections-modern-container > div:nth-child(2) {
+  padding-left: 0;
+}
+#selections-modern-container > div:nth-child(1) > div {
+  border-top-right-radius: 0px!important;
+  border-bottom-right-radius: 0px!important;
+}
+#selections-modern-container > div:nth-child(1) {
+  padding-right: 0;
+}
+  `;
+  window.self.document.body.appendChild(style);
+};
 
 export default Modern;
